f80ae0e45380a54dbef259107c58472956d63201
Andreas Bergström <skrap@andreasb.se>
2024-02-08 20:39:16 +0100

    minor styling changesw

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const API_URL = "https://api.adviceslip.com/advice";
 
 const adviceElement = document.getElementById("advice");
-const adviceNoElement = document.getElementById("adviceID");
+const adviceNrElement = document.getElementById("adviceID");
 
 const buttonDice = document.getElementById("buttonDice");
 
 async function fetchAdvice() {
   const response = await fetch(API_URL);
   const { slip } = await response.json();
-  adviceNoElement.innerText = `ADVICE #${slip.id}`;
+  adviceNrElement.innerText = `ADVICE #${slip.id}`;
   adviceElement.innerText = `"${slip.advice}"`;
 }
 window.addEventListener("load", fetchAdvice);
